209c7214849ace8794526b05a47ece3056435720
h.koga <hiroki1999829@gmail.com>
2025-05-25 18:36:35 +0900

    WPF  ReactiveProperty勉強

---
+++ WpfApp1/ViewModels/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Reactive.Bindings;
+
+namespace WpfApp1.ViewModels
+{
+    class ViewModel
+    {
+        public ReactiveProperty<string> InputValue { get; }
+        public ReactiveProperty<string> OutputValue { get; }
+
+        public ViewModel()
+        {
+            InputValue = new ReactiveProperty<string>();
+            OutputValue = InputValue.ToReactiveProperty();
+        }
+
+    }
+}
