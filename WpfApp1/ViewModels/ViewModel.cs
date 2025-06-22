90cb40009c009e14f7e1ef5e6e8d2c5715356822
h.koga <hiroki1999829@gmail.com>
2025-06-22 21:27:48 +0900

    WPF練習

--- WpfApp1/ViewModels/ViewModel.cs
+++ WpfApp1/ViewModels/ViewModel.cs
@@ -1,17 +1,51 @@
 ﻿using Reactive.Bindings;
+using System.Reactive;
 
 namespace WpfApp1.ViewModels
 {
     class ViewModel
     {
-        public ReactiveProperty<string> InputValue { get; }
-        public ReactiveProperty<string> OutputValue { get; }
+        /// <summary>
+        /// テキストボックスに入力された値を格納する
+        /// </summary>
+        public ReactiveProperty<string> InputValue { get; init; } = new();
+        /// <summary>
+        /// 画面に表示する値を格納する
+        /// </summary>
+        public ReactiveProperty<string> OutputValue { get; init; } = new();
+        /// <summary>
+        /// 表示ボタンのコマンドを格納する
+        /// </summary>
+        public ReactiveCommand DisplayCommand { get; init; } = new();
+		/// <summary>
+		/// 削除ボタンのコマンドを格納する
+		/// </summary>
+		public ReactiveCommand DeleteCommand { get; init; } = new();
 
-        public ViewModel()
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public ViewModel()
         {
             InputValue = new ReactiveProperty<string>();
-            OutputValue = InputValue.ToReactiveProperty();
-        }
+            DisplayCommand.Subscribe(Display);
+			DeleteCommand.Subscribe(Delete);
+		}
 
-    }
+        /// <summary>
+        /// 表示ボタンを押したときの挙動
+        /// </summary>
+        private void Display()
+        {
+            OutputValue.Value = InputValue.Value;
+		}
+		/// <summary>
+		/// 削除ボタンを押したときの挙動
+		/// </summary>
+		private void Delete()
+		{
+			OutputValue.Value = string.Empty;
+		}
+
+	}
 }
